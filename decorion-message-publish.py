14472cfda850252e89cf1f6012cff8cb3c4d498f
1styrGroupB <1styrGroupB@pn.local>
2021-02-25 14:08:26 +0800

    New Commits

---
+++ decorion-message-publish.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import time
+
+topic = "vangie/message"
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    print(" ")
+    print("This is a publisher client")
+    print("Published topic: " + topic)
+    print("Published payload: " + payload)
+    print(" ")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect("test.mosquitto.org", 1883, 60)
+payload = input("Input message here..")
+time.sleep(1)
+while True:
+    client.loop()
+    client.publish(topic, payload)
+    time.sleep(1)
+
